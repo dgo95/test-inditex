d5504b28726da44ad8981c82f0ab642ab9f067f2
Diego Garcia <garcidie@hotmail.com>
2025-02-25 16:12:35 +0100

    feat: define Price entity with JPA and Lombok annotations

---
+++ src/main/java/es/test/inditex/entity/Price.java
@@ -0,0 +1,46 @@
+package es.test.inditex.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "prices")
+public class Price {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "brand_id", nullable = false)
+    private Long brandId;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date", nullable = false)
+    private LocalDateTime endDate;
+
+    @Column(name = "price_list", nullable = false)
+    private Integer priceList;
+
+    @Column(name = "product_id", nullable = false)
+    private Long productId;
+
+    @Column(name = "priority", nullable = false)
+    private Integer priority;
+
+    @Column(name = "price", nullable = false)
+    private BigDecimal price;
+
+    @Column(name = "curr", nullable = false)
+    private String curr;
+}
